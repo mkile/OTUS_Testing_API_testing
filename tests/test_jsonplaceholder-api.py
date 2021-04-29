115b657bc31d762d55c9cde2b03651f1074028e5
Mikhail <mkiles81@gmail.com>
2021-04-29 19:35:14 +0300

    Update test_jsonplaceholder-api.py

--- tests/test_jsonplaceholder-api.py
+++ tests/test_jsonplaceholder-api.py
@@ -16,7 +16,6 @@ def test_get_results_by_name(json_base_url, posts_list, post):
     link = json_base_url + 'posts/' + str(post)
     result = requests.get(link)
     json_data = json.loads(result.content)
-    print('Post number=', post, json_data['title'], ' = ', posts_list[post - 1]['title'])
     assert (json_data['title'] == posts_list[post - 1]['title']) & (result.status_code == 200)
 
 
@@ -29,7 +28,6 @@ def test_update_resource(json_base_url, post):
                 'userId': int(uniform(1, 99))}
     result = requests.put(link, json.dumps(new_data))
     json_data = json.loads(result.content)
-    print('Put post', post, 'returned', json_data['id'])
     assert (json_data['id'] == post) & (result.status_code == 200)
 
 
@@ -37,7 +35,6 @@ def test_update_resource(json_base_url, post):
 def test_delete_resource(json_base_url, post):
     link = json_base_url + 'posts/' + str(post)
     result = requests.delete(link)
-    print('Delete post', post)
     assert result.status_code == 200
 
 
